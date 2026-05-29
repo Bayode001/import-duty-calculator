5bf910ccb4f83504f6cbe4e013513573bcc4d4d7
Bayode <kunleaseperi@gmail.com>
2026-05-29 08:31:02 +0100

    updates
    Add reset password routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-// Temporarily replace your App.js with this to test
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
 import ResetPassword from './components/ResetPassword';
+import AppContent from './AppContent';
+import './App.css';
 
 function App() {
-  return <ResetPassword />;
+  return (
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<AppContent />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
+    </AuthProvider>
+  );
 }
 
 export default App;
